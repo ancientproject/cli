94fb46fd5cc5ed5944bd5cea5b7f8b0ad649727d
Yuuki Wesp <ls-micro@ya.ru>
2020-04-13 09:50:49 +0300

    add additional methods for resulting

--- cmd/Internal/RuneCommand.cs
+++ cmd/Internal/RuneCommand.cs
@@ -15,23 +15,34 @@
 
             var app = cmd.Setup();
 
-
             try
             {
                 return await app.Execute(args);
             }
             catch (CommandParsingException parsingException)
             {
-                Console.WriteLine(parsingException.Message.Color(Color.Red));
-                return 1;
+                return await Fail(parsingException.Message);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.ToString().Color(Color.Red));
-                return 1;
+                return await Fail(ex.ToString());
             }
         }
 
         internal abstract CommandLineApplication Setup();
+
+
+        protected static Task<int> Success() => Task.FromResult(0);
+        protected static Task<int> Fail() => Task.FromResult(1);
+        protected static Task<int> Fail(string text)
+        {
+            Console.WriteLine($"{":x:".Emoji()} {text.Color(Color.Red)}");
+            return Fail();
+        }
+        protected static Task<int> Success(string text)
+        {
+            Console.WriteLine($"{":heavy_check_mark:".Emoji()} {text.Color(Color.GreenYellow)}");
+            return Success();
+        }
     }
 }
