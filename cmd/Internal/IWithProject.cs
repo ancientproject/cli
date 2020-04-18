69481c578e1ddbeedf3853dfffb77d1178b088bf
Yuuki Wesp <ls-micro@ya.ru>
2020-04-18 13:29:58 +0300

    add validation and parsing spec file

--- cmd/Internal/IWithProject.cs
+++ cmd/Internal/IWithProject.cs
@@ -3,7 +3,10 @@
     using System;
     using System.Drawing;
     using System.IO;
+    using System.Linq;
+    using Ancient.ProjectSystem;
     using etc;
+    using Newtonsoft.Json;
 
     public interface IWithProject
     {
@@ -26,6 +29,53 @@
                 Console.WriteLine($"{":fried_shrimp:".Emoji()} {"Specify".Nier().Color(Color.Red)} which project file to use because this folder contains more than one project file..");
                 return false;
             }
+            return true;
+        }
+        public static bool ValidateRuneSpec(this IWithProject _, string directory, out RuneSpec specFile, out string path)
+        {
+            var projectFiles = Directory.GetFiles(directory, "*.rspec.json");
+            specFile = default;
+            path = string.Empty;
+            if (projectFiles.Length == 0)
+            {
+                Console.WriteLine($"{":trident:".Emoji()} {"Couldn't".Nier().Color(Color.Red)} find a rune package spec. Ensure a project specification in {directory}.");
+                return false;
+            }
+            if (projectFiles.Length > 1)
+            {
+                Console.WriteLine($"{":trident:".Emoji()} {"Specify".Nier().Color(Color.Red)} which spec file to use because this folder contains more than one spec file..");
+                return false;
+            }
+
+            path = projectFiles.Single();
+
+            try
+            {
+                specFile = JsonConvert.DeserializeObject<RuneSpec>(File.ReadAllText(path));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine($"{":trident:".Emoji()} {"Fail".Nier(1).Color(Color.Red)} parse specification file...");
+                return false;
+            }
+
+            // TODO regex rules
+            if (string.IsNullOrEmpty(specFile.ID))
+            {
+                Console.WriteLine($"{":trident:".Emoji()} [{"Validate".Nier(4).Color(Color.Red)}] ID can't empty.");
+                return false;
+            }
+
+            if (specFile.Version is null)
+            {
+                Console.WriteLine($"{":trident:".Emoji()} [{"Validate".Nier(4).Color(Color.Red)}] Version can't empty.");
+                return false;
+            }
+
+
+
+
             return true;
         }
     }
