7da1b447552b50404b98e2b6a5a8faa51513740c
Yuuki Wesp <ls-micro@ya.ru>
2020-04-26 17:43:16 +0300

    Update getBinary.js

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -2,7 +2,7 @@ const Binary = require('./binary');
 function getBinary() {
     const name = 'rune';
     const version = require('../package.json').version;
-    const url = `https://github.com/ancientproject/cli/releases/download/v${version}/${name}-cli-win64.zip`;
+    const url = `https://github.com/ancientproject/cli/releases/download/v${version}/${name}-cli-win-64.zip`;
     return new Binary(url, { name });
 }
 
