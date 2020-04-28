651feb791a9193d8531aa70493baebba2dda71e1
Yuuki Wesp <ls-micro@ya.ru>
2020-04-28 23:10:17 +0300

    add vm path and acc path

--- src/etc/Dirs.cs
+++ src/etc/Dirs.cs
@@ -9,12 +9,20 @@
             new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".rune"));
         public static FileInfo ConfigFile =>
             new FileInfo(Path.Combine(RootFolder.FullName, "@.ini"));
+        public static DirectoryInfo VMFolder 
+            => new DirectoryInfo(Path.Combine(RootFolder.FullName, "vm"));
+        public static DirectoryInfo CompilerFolder
+            => new DirectoryInfo(Path.Combine(RootFolder.FullName, "acc"));
 
 
         public static void Ensure()
         {
             if(!RootFolder.Exists)
                 RootFolder.Create();
+            if (!VMFolder.Exists)
+                VMFolder.Create();
+            if (!CompilerFolder.Exists)
+                CompilerFolder.Create();
         }
     }
 }
