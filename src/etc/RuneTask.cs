de097ea4b17ef380e7c389e059d246ef931da2cc
Yuuki Wesp <ls-micro@ya.ru>
2020-04-29 04:47:31 +0300

    add overloading for rune task

--- src/etc/RuneTask.cs
+++ src/etc/RuneTask.cs
@@ -6,6 +6,28 @@
 
     public class RuneTask
     {
+        public static async ValueTask Fire(Action target) =>
+            await Fire<int>(async () =>
+            {
+                await Task.Delay(1);
+                try
+                {
+                    target();
+                    return 0;
+                }
+                catch (Exception e)
+                {
+                    return 1;
+                }
+            }, _ => _ == 0);
+
+        public static async ValueTask<T> Fire<T>(Func<T> target, Func<T, bool> successPredicate) =>
+            await Fire<T>(async () =>
+            {
+                await Task.Delay(1);
+                return target();
+            }, successPredicate);
+
         public static async ValueTask<T> Fire<T>(Func<Task<T>> target, Func<T, bool> successPredicate)
         {
             var task = target();
