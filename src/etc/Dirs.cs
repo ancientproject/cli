e04d059e2e9cad230c769df5ecc654e1a99268e8
Yuuki Wesp <ls-micro@ya.ru>
2020-04-29 04:47:56 +0300

    add additional folders and binaries into

--- src/etc/Dirs.cs
+++ src/etc/Dirs.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using System.Runtime.InteropServices;
 
     public static class Dirs
     {
@@ -13,7 +14,21 @@
             => new DirectoryInfo(Path.Combine(RootFolder.FullName, "vm"));
         public static DirectoryInfo CompilerFolder
             => new DirectoryInfo(Path.Combine(RootFolder.FullName, "acc"));
+        public static DirectoryInfo CacheFolder
+            => new DirectoryInfo(Path.Combine(RootFolder.FullName, ".cache"));
 
+
+        public static class Bin
+        {
+            public static FileInfo VM =>
+                RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? 
+                    new FileInfo(Path.Combine(VMFolder.FullName, "vm.exe")) : 
+                    new FileInfo(Path.Combine(VMFolder.FullName, "vm"));
+            public static FileInfo ACC =>
+                RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
+                    new FileInfo(Path.Combine(CompilerFolder.FullName, "acc.exe")) :
+                    new FileInfo(Path.Combine(CompilerFolder.FullName, "acc"));
+        }
 
         public static void Ensure()
         {
@@ -23,6 +38,8 @@
                 VMFolder.Create();
             if (!CompilerFolder.Exists)
                 CompilerFolder.Create();
+            if (!CacheFolder.Exists)
+                CacheFolder.Create();
         }
     }
 }
